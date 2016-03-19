b1b442c449a3e32730aaf24696e50802af930d16
Shashi Lo <shashilo0428@gmail.com>
2016-03-18 22:51:53 -0500

    Initial GIT setup

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+var gulp = require('gulp'),
+    autoprefixer = require('autoprefixer')
+    bower = require('gulp-bower')
+    bowerNormalizer = require('gulp-bower-normalize')
+    livereload = require('gulp-livereload')
+    notify = require("gulp-notify")
+    postcss = require('gulp-postcss')
+    sass = require('gulp-ruby-sass')
+    sourcemaps = require('gulp-sourcemaps');
+
+// Set minifier to false to keep Sass sourcemaps support
+var postcssOptions = {
+    optimizers: {
+        "autoprefixer": true,
+        "minifier": true
+    }
+};
+
+var config = {
+     sassPath: 'sass',
+     bowerDir: 'bower_components'
+}
+
+gulp.task('bower', function() {
+    return bower()
+         .pipe(gulp.dest(config.bowerDir)) 
+});
+
+// Run sass compiler with Pleeease post compiler
+gulp.task('sass', function() {
+    return sass('themes/custom/microt/scss/*.scss', {
+      sourcemap: true,
+      style: "nested",
+              loadPath: [
+                  'sass',
+             config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+             config.bowerDir + '/normalize-scss/sass',
+             config.bowerDir + '/support-for/sass',
+             config.bowerDir + '/font-awesome/scss',
+              ]
+     })
+    .on("error", notify.onError(function (error) {
+        return "Error: " + error.message;
+     }))
+
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+    .pipe(bowerNormalizer({bowerJson: './bower.json'}))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('themes/custom/microt/'))
+    .pipe(livereload());
+});
+
+gulp.task('sass_prod', function() { 
+    return sass('themes/custom/microt/scss/*.scss', {
+      sourcemap: false,
+      style: "compressed",
+              loadPath: [
+                  'sass',
+             config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+             config.bowerDir + '/normalize-scss/sass',
+             config.bowerDir + '/support-for/sass'
+              ]
+     })
+    .on("error", notify.onError(function (error) {
+         return "Error: " + error.message;
+    }))
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+    .pipe(gulp.dest('themes/custom/microt/')); 
+});
+
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('themes/custom/microt/scss/**/*.scss', ['sass']);
+});
+
+// Build Tasks
+gulp.task('default', ['sass', 'bower', 'watch']);
+gulp.task('prod', ['sass_prod']);
