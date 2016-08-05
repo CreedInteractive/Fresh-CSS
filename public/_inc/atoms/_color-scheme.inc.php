1fa642228959f2ba9e8f1e6334d1df323a5bce7a
Garrick Jacobson <garrick@creedinteractive.com>
2016-08-05 12:55:49 -0500

    working sass compilng

--- public/_inc/atoms/_color-scheme.inc.php
+++ public/_inc/atoms/_color-scheme.inc.php
@@ -1,4 +1,11 @@
 <section id="color-scheme">
     <h2>Colors</h2>
     <div class="sg-color sg-color--a"><span class="sg-color-swatch"><span class="sg-animated">#0075bc</span></span></div>
+    <div class="sg-color sg-color--b"><span class="sg-color-swatch"><span class="sg-animated"></span></span></div>
+    <div class="sg-color sg-color--c"><span class="sg-color-swatch"><span class="sg-animated"></span></span></div>
+    <div class="sg-color sg-color--d"><span class="sg-color-swatch"><span class="sg-animated"></span></span></div>
+    <div class="sg-color sg-color--e"><span class="sg-color-swatch"><span class="sg-animated"></span></span></div>
+    <div class="sg-color sg-color--f"><span class="sg-color-swatch"><span class="sg-animated"></span></span></div>
+    <div class="sg-color sg-color--g"><span class="sg-color-swatch"><span class="sg-animated"></span></span></div>
+    <div class="sg-color sg-color--h"><span class="sg-color-swatch"><span class="sg-animated"></span></span></div>
 </section>
