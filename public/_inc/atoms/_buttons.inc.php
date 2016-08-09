30bb3b16a1b9cebed6f04b59ee31277cd18554c1
Shashi Lo <shashi@creedinteractive.com>
2016-08-09 15:53:20 -0500

    Add green button and display correct typography fonts

--- public/_inc/atoms/_buttons.inc.php
+++ public/_inc/atoms/_buttons.inc.php
@@ -2,19 +2,23 @@
     <button type="button" class="btn btn-default btn-lg">Large button</button>
     <button type="button" class="btn btn-primary btn-lg">Large button</button>
     <button type="button" class="btn btn-secondary btn-lg">Large button</button>
+    <button type="button" class="btn btn-success btn-lg">Large button</button>
 </p>
 <p>
     <button type="button" class="btn btn-default">Default button</button>
     <button type="button" class="btn btn-primary">Default button</button>
     <button type="button" class="btn btn-secondary">Default button</button>
+    <button type="button" class="btn btn-success">Default button</button>
 </p>
 <p>
     <button type="button" class="btn btn-default btn-sm">Small button</button>
     <button type="button" class="btn btn-primary btn-sm">Small button</button>
     <button type="button" class="btn btn-secondary btn-sm">Small button</button>
+    <button type="button" class="btn btn-success btn-sm">Small button</button>
 </p>
 <p>
     <button type="button" class="btn btn-default btn-xs">Extra small button</button>
     <button type="button" class="btn btn-primary btn-xs">Extra small button</button>
     <button type="button" class="btn btn-secondary btn-xs">Extra small button</button>
+    <button type="button" class="btn btn-success btn-xs">Extra small button</button>
 </p>
