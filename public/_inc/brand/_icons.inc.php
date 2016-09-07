d207b190fe2199b08a6b16b8f7560f98060a0e4a
Shashi Lo <shashi@creedinteractive.com>
2016-09-07 00:01:05 -0500

    Adding more icons to the style guide

--- public/_inc/brand/_icons.inc.php
+++ public/_inc/brand/_icons.inc.php
@@ -1,68 +1,173 @@
-<ul class="icon-fonts">
-    <li>
-        <span class="icon-icon-close-subscore">
-            <span class="path1"></span><span class="path2"></span><span class="path3"></span>
-        </span>
-    </li>
-    <li>
-        <span class="icon-icon-expand-collapse-UTnav">
-            <span class="path1"></span><span class="path2"></span><span class="path3"></span>
-        </span>
-    </li>
-    <li>
-        <span class="icon-icon-flag"></span>
-    </li>
-    <li>
-        <span class="icon-icon-settings"></span>
-    </li>
-    <li>
-        <span class="icon-icon-schedule"></span>
-    </li>
-    <li>
-        <span class="icon-icon-checkmark"></span>
-    </li>
-    <li>
-        <span class="icon-icon-open-dropdown-menu"></span>
-    </li>
-    <li>
-        <span class="icon-icon-close-panel"></span>
-    </li>
-    <li>
-        <span class="icon-icon-expand-accordion">
-            <span class="path1"></span><span class="path2"></span>
-        </span>
-    </li>
-    <li>
-        <span class="icon-icon-collapse-accordion">
-            <span class="path1"></span><span class="path2"></span>
-        </span>
-    </li>
-    <li>
-        <span class="icon-icon-cross-out"></span>
-    </li>
-    <li>
-        <span class="icon-icon-labs"></span>
-    </li>
-    <li>
-        <span class="icon-icon-next-arrow-carousel">
-            <span class="path1"></span><span class="path2"></span>
-        </span>
-    </li>
-    <li>
-        <span class="icon-icon-back-arrow-carousel">
-            <span class="path1"></span><span class="path2"></span>
-        </span>
-    </li>
-    <li>
-        <span class="icon-icon-image-zoom"></span>
-    </li>
-    <li>
-        <span class="icon-icon-highlighter"></span>
-    </li>
-    <li>
-        <span class="icon-icon-favorites"></span>
-    </li>
-    <li>
-        <span class="icon-icon-feedback"></span>
-    </li>
-</ul>
+<div class="icon-fonts">
+
+    <p>It's easiest if you use your browser inspector to find the icon element you need. Then, you can copy and paste the HTML code for that icon.</p>
+
+    <span class="icon-icon-close-subscore">
+        <span class="path1"></span><span class="path2"></span><span class="path3"></span>
+    </span>
+
+    <span class="icon-icon-expand-collapse-UTnav">
+        <span class="path1"></span><span class="path2"></span><span class="path3"></span>
+    </span>
+
+    <span class="icon-icon-flag"></span>
+
+    <span class="icon-icon-settings"></span>
+
+    <span class="icon-icon-schedule"></span>
+
+    <span class="icon-icon-checkmark"></span>
+
+    <span class="icon-icon-open-dropdown-menu"></span>
+
+    <span class="icon-icon-close-panel"></span>
+
+    <span class="icon-icon-expand-accordion">
+        <span class="path1"></span><span class="path2"></span>
+    </span>
+
+    <span class="icon-icon-collapse-accordion">
+        <span class="path1"></span><span class="path2"></span>
+    </span>
+
+    <span class="icon-icon-cross-out"></span>
+
+    <span class="icon-icon-labs"></span>
+
+    <span class="icon-icon-next-arrow-carousel">
+        <span class="path1"></span><span class="path2"></span>
+    </span>
+
+    <span class="icon-icon-back-arrow-carousel">
+        <span class="path1"></span><span class="path2"></span>
+    </span>
+
+    <span class="icon-icon-image-zoom"></span>
+
+    <span class="icon-icon-highlighter"></span>
+
+    <span class="icon-icon-favorites"></span>
+
+    <span class="icon-icon-feedback"></span>
+
+    <span class="icon-icon-tenant-settings"></span>
+
+    <span class="icon-icon-syllabus"></span>
+
+    <span class="icon-icon-cme"></span>
+
+    <span class="icon-icon-send"></span>
+
+    <span class="icon-icon-copy"></span>
+
+    <span class="icon-icon-clone"></span>
+
+    <span class="icon-icon-send-stamp"></span>
+
+    <span class="icon-icon-critique"></span>
+
+    <span class="icon-icon-item-pool"></span>
+
+    <span class="icon-icon-content-coverage"></span>
+
+    <span class="icon-icon-reference-data-management"></span>
+
+    <span class="icon-icon-roles-permissions"></span>
+
+    <span class="icon-icon-item-bank"></span>
+
+    <span class="icon-icon-log-out">
+        <span class="path1"></span><span class="path2"></span><span class="path3"></span>
+    </span>
+
+    <span class="icon-icon-logged-in">
+        <span class="path1"></span><span class="path2"></span><span class="path3"></span>
+    </span>
+
+    <span class="icon-icon-review"></span>
+
+    <span class="icon-icon-loading"></span>
+
+    <span class="icon-icon-group-management"></span>
+
+    <span class="icon-icon-drag-drop"></span>
+
+    <span class="icon-icon-user-management"></span>
+
+    <span class="icon-icon-x"></span>
+
+    <span class="icon-icon-media-library"></span>
+
+    <span class="icon-icon-question-admin"></span>
+
+    <span class="icon-icon-import"></span>
+
+    <span class="icon-icon-required-item"></span>
+
+    <span class="icon-icon-asterisk"></span>
+
+    <span class="icon-icon-manage-project"></span>
+
+    <span class="icon-icon-edit"></span>
+
+    <span class="icon-icon-notes"></span>
+
+    <span class="icon-icon-scratch-pad"></span>
+
+    <span class="icon-icon-calculator2"></span>
+
+    <span class="icon-icon-item-creation"></span>
+
+    <span class="icon-icon-publish"></span>
+
+    <span class="icon-icon-reports"></span>
+
+    <span class="icon-icon-save"></span>
+
+    <span class="icon-icon-assessment-settings"></span>
+
+    <span class="icon-icon-profile"></span>
+
+    <span class="icon-icon-ticket"></span>
+
+    <span class="icon-icon-compass-arrow"></span>
+
+    <span class="icon-icon-phone-bars"></span>
+
+    <span class="icon-icon-warning"></span>
+
+    <span class="icon-icon-filters"></span>
+
+    <span class="icon-icon-chat"></span>
+
+    <span class="icon-icon-question"></span>
+
+    <span class="icon-icon-undo"></span>
+
+    <span class="icon-icon-repeat"></span>
+
+    <span class="icon-icon-admin"></span>
+
+    <span class="icon-icon-question-comment"></span>
+
+    <span class="icon-icon-lock-open"></span>
+
+    <span class="icon-icon-lock-closed"></span>
+
+    <span class="icon-icon-support"></span>
+
+    <span class="icon-icon-content-dev"></span>
+
+    <span class="icon-icon-resources"></span>
+
+    <span class="icon-icon-assessment"></span>
+
+    <span class="icon-icon-dashboard"></span>
+
+    <span class="icon-icon-hide"></span>
+
+    <span class="icon-icon-show"></span>
+
+    <span class="icon-icon-remove-delete"></span>
+
+</div>
